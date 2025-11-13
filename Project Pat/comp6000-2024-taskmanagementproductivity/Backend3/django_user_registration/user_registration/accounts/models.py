a7ba9f589540691f07ddcb80a36a8a9f6b137a8d
ToluOsh <toluoshad@gmail.com>
2025-11-13 19:09:48 +0000

    Delete Project Pat/comp6000-2024-taskmanagementproductivity directory

--- Project Pat/comp6000-2024-taskmanagementproductivity/Backend3/django_user_registration/user_registration/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):  # Extending Django's built-in user model
-    email = models.EmailField(unique=True)  # Make email unique
-
-    def __str__(self):
-        return self.username
--- Project Pat/comp6000-2024-taskmanagementproductivity/Backend3/django_user_registration/user_registration/tasks/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class TasksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
-
-    def ready(self):
-        import tasks.signals  # 👈 this is the key
